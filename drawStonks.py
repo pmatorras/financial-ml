a56695b830dce2fe39818cb45c06c3a0133ae937
pmatorras <pablo.matorras@cern.ch>
2021-07-01 19:28:44 +0200

    cleanup draw

--- drawStonks.py
+++ drawStonks.py
@@ -10,11 +10,6 @@ stocknms = []
 for stock in  stocks["abrv"]:
     if stock not in stocknms: stocknms.append(stock)
 
-print stocks.columns
-
-print bool('act_val' == stocks["variable"][0])
-print  stocks[stocks["variable"] == "act_val"] 
-#print [stocks["variable"]=="act_val"]
 
 act_val = stocks[stocks["variable"]=="act_val"][today]
 exp_val = stocks[stocks["variable"]=="exp_val"][today]
@@ -26,14 +21,14 @@ exp_relval    = 100*(exp_val.reset_index()/act_val.reset_index())[today]
 exp_mindif    = exp_val.reset_index()-exp_min.reset_index()
 exp_maxdif    = exp_max.reset_index()-exp_val.reset_index()
 exp_minreldif = 100*exp_mindif.reset_index()/act_val.reset_index()
-exp_maxreldif = 100*exp_mindif.reset_index()/act_val.reset_index()
+exp_maxreldif = 100*exp_maxdif.reset_index()/act_val.reset_index()
 
 exp_ran    = np.array(list(zip(exp_mindif[today], exp_maxdif[today]))).T
 exp_relran = np.array(list(zip(exp_minreldif[today], exp_maxreldif[today]))).T
 
 difference = act_val.reset_index() -exp_val.reset_index()
-colors = []
-print difference
+colors     = []
+
 for dif in difference[today]:
     if dif<0: colors.append('g')
     else: colors.append('r')
@@ -49,10 +44,8 @@ plt.tick_params(axis='x', rotation=45)
 plt.savefig("absolute_difference.pdf")
 
 plt.clf()
-#print len(stocknms), len(act_relval), len(colors)
 
 print exp_relval, "\n neew", stocknms
-#plt.axhline(y=1)
 plt.scatter(stocknms, act_relval, marker="_",label="Stock value at "+today, color=colors)
 plt.errorbar(stocknms, exp_relval, yerr =exp_relran, fmt='.', color='blue', label="expected value")
 plt.ylabel("Percentage change")
@@ -60,5 +53,4 @@ plt.xlabel("Stock name")
 plt.title("Relative expected gain/loss")
 plt.legend()
 plt.tick_params(axis='x', rotation=45)
-
 plt.savefig("relative_difference.pdf")
