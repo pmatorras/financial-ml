d3025a0f28fbfb874a2e17309347b72e3f87aa33
pmatorras <pablo.matorras@cern.ch>
2021-07-14 21:05:13 +0200

    fix relative errobars and cleanup

--- drawStonks.py
+++ drawStonks.py
@@ -22,23 +22,29 @@ act_val    = stocks[stocks["variable"]=="act_val"][today]
 exp_val    = stocks[stocks["variable"]=="exp_val"][today]
 exp_max    = stocks[stocks["variable"]=="exp_max"][today]
 exp_min    = stocks[stocks["variable"]=="exp_min"][today]
-exp_mindif = exp_val.reset_index()-exp_min.reset_index()
-exp_maxdif = exp_max.reset_index()-exp_val.reset_index()
-exp_ran    = np.array(list(zip(exp_mindif[today], exp_maxdif[today]))).T
+exp_mindif = exp_val.reset_index(drop=True)-exp_min.reset_index(drop=True)
+exp_maxdif = exp_max.reset_index(drop=True)-exp_val.reset_index(drop=True)
+exp_ran    = np.array(list(zip(exp_mindif, exp_maxdif))).T
 
 act_relval    = -100 + 100*act_val/act_val
-exp_relval    = -100 + 100*(exp_val.reset_index()/act_val.reset_index())[today]
-exp_minreldif = -100 + 100*exp_mindif.reset_index()/act_val.reset_index()
-exp_maxreldif = -100 + 100*exp_maxdif.reset_index()/act_val.reset_index()
-exp_relran     = np.array(list(zip(exp_minreldif[today], exp_maxreldif[today]))).T
+exp_relval    = -100 + 100*(exp_val.reset_index(drop=True)/act_val.reset_index(drop=True))
+exp_minreldif = 100*exp_mindif.reset_index(drop=True)/act_val.reset_index(drop=True)
+exp_maxreldif = 100*exp_maxdif.reset_index(drop=True)/act_val.reset_index(drop=True)
+exp_relran    = np.array(list(zip(exp_minreldif, exp_maxreldif))).T
 
 #Get colours
 difference = act_val.reset_index() -exp_val.reset_index()
 colors     = []
+longcolors = []
 for dif in difference[today]:
-    if dif<0: colors.append('g')
-    else: colors.append('r')
+    if dif<0:
+        colors.append('g')
+        longcolors.append([0,1,0,1])
+    else:
+        colors.append('r')
+        longcolors.append([1,0,0,1])
 
+#absolute difference
 plt.gcf().subplots_adjust(bottom=0.15)
 plt.scatter(stocknms, act_val, marker="_",label="Stock value at "+today, color=colors)
 plt.errorbar(stocknms, exp_val, yerr =exp_ran, fmt='.', color='blue', label="expected value")
@@ -51,9 +57,10 @@ plt.savefig(plotdir+"absolute_difference.pdf")
 
 plt.clf()
 
-print exp_relval, "\n neew", stocknms
+#Relative difference
 plt.scatter(stocknms, act_relval, marker="_",label="Stock value at "+today, color=colors)
-plt.errorbar(stocknms, exp_relval, yerr =exp_relran, fmt='.', color='blue', label="expected value")
+plt.errorbar(stocknms, exp_relval, yerr =exp_relran, color='blue', label="expected value", linestyle="none", marker ="s")
+
 plt.ylabel("Exp. gain [%]")
 plt.xlabel("Stock name")
 plt.title("Relative expected gain/loss")
