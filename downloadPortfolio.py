eeb353727daf1e619684a498572f0e58877b3cea
pmatorras <pablo.matorras@cern.ch>
2021-12-01 12:29:35 +0100

    update code to accomodate new items

--- downloadPortfolio.py
+++ downloadPortfolio.py
@@ -1,84 +1,87 @@
-import requests, json
+import requests, json, os, sys
+foldir  = os.path.dirname(sys.argv[0])
+if "/" in foldir: foldir += "/"
+datadir = foldir + "Data/"
 
-#Retrieve username and pasword from config file
-with open('config', 'r') as configFile:
-    CONFIG = json.load(configFile)
+if __name__ == '__main__':
 
-
-# login
-URL_LOGIN = "https://trader.degiro.nl/login/secure/login"
-payload   = {'username': CONFIG['username'],
-             'password': CONFIG['password'],
-             'isPassCodeReset': False,
-             'isRedirectToMobile': False}
-header    = {'content-type': 'application/json'}
-rlogin    = requests.post(URL_LOGIN, headers=header, data=json.dumps(payload))
-sessionID = rlogin.json()["sessionId"]
-
-# get int account
-URL_CLIENT = 'https://trader.degiro.nl/pa/secure/client'
-payload    = {'sessionId': sessionID}
-rclient    = requests.get(URL_CLIENT, params=payload)
-intAccount = rclient.json()["data"]["intAccount"]
-
-#Retrieve data
-URL = "https://trader.degiro.nl/trading/secure/v5/update/"+str(intAccount)+";jsessionid="+sessionID
-payload = {'intAccount': intAccount,
-           'sessionId': sessionID,
-           'cashFunds': 0,
-           'orders': 0,
-           'portfolio': 0,
-           'totalPortfolio': 0,
-           'historicalOrders': 0,
-           'transactions': 0,
-           'alerts': 0}
-rcash = requests.get(URL, params=payload)
-data  = rcash.json()
-
-# get cashfund
-cashfund = {}
-for currency in data["cashFunds"]["value"]:
-    for parameter in currency["value"]:
-        if parameter["name"] == "currencyCode":
-            code = parameter["value"]
-        if parameter["name"] == "value":
-            value = parameter["value"]
-    cashfund[code] = value
+    #Retrieve username and pasword from config file
+    with open('config', 'r') as configFile:
+        CONFIG = json.load(configFile)
 
 
-print cashfund
+    # login
+    URL_LOGIN = "https://trader.degiro.nl/login/secure/login"
+    payload   = {'username': CONFIG['username'],
+                 'password': CONFIG['password'],
+                 'isPassCodeReset': False,
+                 'isRedirectToMobile': False}
+    header    = {'content-type': 'application/json'}
+    rlogin    = requests.post(URL_LOGIN, headers=header, data=json.dumps(payload))
+    sessionID = rlogin.json()["sessionId"]
 
-## get portfolio
-temp_portfolio = []
-for position in data["portfolio"]["value"]:
-    to_append = {}
-    for position_data in position["value"]:
-        if "value" in position_data:
-            to_append[position_data["name"]] = position_data["value"]
-    temp_portfolio.append(to_append)
-portfolio = list(filter(lambda x: x["positionType"] == "PRODUCT" and x["size"]>0 , temp_portfolio))
+    # get int account
+    URL_CLIENT = 'https://trader.degiro.nl/pa/secure/client'
+    payload    = {'sessionId': sessionID}
+    rclient    = requests.get(URL_CLIENT, params=payload)
+    intAccount = rclient.json()["data"]["intAccount"]
 
-BEP = {}
-for fund in portfolio:
-    BEP[fund["id"]] = { "BEP"       : fund["breakEvenPrice"],
-                        "size"      :  fund['size'],
-                        'price'     : fund['price'],
-                        'gain/loss' : round(abs(fund['todayPlBase']['EUR'])-abs(fund['plBase']['EUR']),4)}#float(fund['value'])-float(fund['plBase']) }
+    #Retrieve data
+    URL = "https://trader.degiro.nl/trading/secure/v5/update/"+str(intAccount)+";jsessionid="+sessionID
+    payload = {'intAccount': intAccount,
+               'sessionId': sessionID,
+               'cashFunds': 0,
+               'orders': 0,
+               'portfolio': 0,
+               'totalPortfolio': 0,
+               'historicalOrders': 0,
+               'transactions': 0,
+               'alerts': 0}
+    rcash = requests.get(URL, params=payload)
+    data  = rcash.json()
+
+    # get cashfund
+    cashfund = {}
+    for currency in data["cashFunds"]["value"]:
+        for parameter in currency["value"]:
+            if parameter["name"] == "currencyCode":
+                code = parameter["value"]
+            if parameter["name"] == "value":
+                value = parameter["value"]
+        cashfund[code] = value
 
 
-## get product info
-url        = "https://trader.degiro.nl/product_search/secure/v5/products/info"
-payload    = {'intAccount': intAccount, 'sessionId': sessionID}
-pid        = [x["id"] for x in portfolio]
-r          = requests.post(url, headers=header, params=payload, data=json.dumps(pid))
-extra_info = r.json()
+    ## get portfolio
+    temp_portfolio = []
+    for position in data["portfolio"]["value"]:
+        to_append = {}
+        for position_data in position["value"]:
+            if "value" in position_data:
+                to_append[position_data["name"]] = position_data["value"]
+        temp_portfolio.append(to_append)
 
-for add_i in extra_info["data"]:
-    for key in BEP[add_i]:
-        extra_info["data"][add_i][key] = BEP[add_i][key]
+    portfolio = list(filter(lambda x: x["positionType"] == "PRODUCT" and x["size"]>0 , temp_portfolio))
 
 
-#save to json
-with open('full_portfolio.json', 'w') as f:
-    json.dump(extra_info["data"], f, indent=4)
+    BEP = {}
+    for fund in portfolio:
+        BEP[fund["id"]] = { "BEP"       : fund["breakEvenPrice"],
+                            "size"      :  fund['size'],
+                            'price'     : fund['price'],
+                            'gain/loss' : round(abs(fund['todayPlBase']['EUR'])-abs(fund['plBase']['EUR']),4)}
+    #product info
+    url        = "https://trader.degiro.nl/product_search/secure/v5/products/info"
+    payload    = {'intAccount': intAccount, 'sessionId': sessionID}
+    pid        = [x["id"] for x in portfolio]
+    r          = requests.post(url, headers=header, params=payload, data=json.dumps(pid))
+    extra_info = r.json()
+
+    for add_i in extra_info["data"]:
+        for key in BEP[add_i]:
+            extra_info["data"][add_i][key] = BEP[add_i][key]
+
+
+    #save to json
+    with open(datadir+'full_portfolio.json', 'w') as f:
+        json.dump(extra_info["data"], f, indent=4)
 
--- updatePortfolio.py
+++ updatePortfolio.py
@@ -6,23 +6,13 @@ from bs4          import BeautifulSoup
 from datetime     import datetime
 from googlesearch import search
 
-now    = datetime.now()
-today  = datetime.date(now)
-foldir = os.path.dirname(sys.argv[0])
+
+foldir  = os.path.dirname(sys.argv[0])
 if "/" in foldir: foldir += "/"
+datadir = foldir + "Data/" 
 
-
-CRED = '\033[91m'
-CEND = '\033[0m'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:63.0) Gecko/20100101 Firefox/63.0'}
-stocks_esp = {"LOGISTA"             : ["logista_hlgd_sa"       ,"56747"],
-              "ORYZON GENOMICS"     : ["oryzon_genomics_sa"    ,"57000"] ,
-              "GRENERGY RENOVABLES" : ["grenergy_renovables_sa","56988"]}
-
-linkbase   = {"cnn" : "https://money.cnn.com/quote/forecast/forecast.html?symb=",
-              "wsj" : "https://www.wsj.com/market-data/quotes/",
-              "esp" : "https://cincodias.elpais.com/mercados/empresas/"}
 
 
 def makeSoup(link):
@@ -32,49 +22,61 @@ def makeSoup(link):
 
 
 
-jsonDict  = open("full_portfolio.json", "rb")
-portfolio = json.load(jsonDict)
-jsonDict.close()
-stocks_csv={}
 def loopqueries(query,recom,links):
-    for j in search(query, tld='com', num =2, stop=2, pause =2):
+    for j in search(query, tld='com', num =1, stop=1, pause =1):
         if recom.lstrip() not in j: continue 
         links.append(j)
 
 def getLinksGoogle(site, stock, recom):
-    links = []
-    query = site+ stock["symbol"]+" "+stock["name"].replace("SA","")+" "+recom
-    print "Stock", stock["symbol"], "\t query", query
+    links  = []
+    symb_i = stock["symbol"]
+    name_i = stock["name"  ].replace("SA","")
+    query  = site+ symb_i+" "+name_i+" "+recom
+    print "Stock", symb_i, "\t query", query
 
     loopqueries(query,recom,links)
-
     if len(links) == 0 :
-        query = site + stock["symbol"].replace("SA","")+ recom 
+        query = site + symb_i + recom 
         loopqueries(query,recom, links)
     if len(links) == 0 :
-        query = site + stock["name"].replace("SA","")+ recom 
+        query = site + name_i + recom 
         loopqueries(query,recom, links)
+    if len(links) == 0 :
+        key_w = None
+        for word in name_i.split(' '):
+            if symb_i in word.upper(): key_w = word
+        if key_w:
+            query = site+key_w+recom
+            loopqueries(query,recom, links)
+        print links, key_w
+            
     print "links", links[0]
     return links
-    
-
-for entry in portfolio:
-    if "IE" in portfolio[entry]["isin"]:
-        print "ETF", entry
-        continue
-
-    stocks_csv[portfolio[entry]["symbol"]] =  [portfolio[entry]["name"], portfolio[entry]["isin"]]
-    
-    if "ES" in portfolio[entry]["isin"]:
-        links = getLinksGoogle("site:cincodias.elpais.com/mercados/empresas/ ", portfolio[entry], " recomendaciones")
-        stocks_csv[portfolio[entry]["symbol"]].append(links[0])
-    else: # "US" in portfolio[entry]["isin"]:
-        links = getLinksGoogle("site:https://www.wsj.com/market-data/quotes/ ", portfolio[entry], " research-ratings")
-        stocks_csv[portfolio[entry]["symbol"]].append(links[0])
-        
 
 
+if __name__ == '__main__':
+    stocks_csv = {}
+    jsonDict   = open(datadir+"full_portfolio.json", "rb")
+    portfolio  = json.load(jsonDict)
+    jsonDict.close()
 
-f = open(foldir+"Portfolio_dict.pkl","wb")
-pickle.dump(stocks_csv,f)
-f.close()
+    for entry in portfolio:
+        if "ETF" in portfolio[entry]["productType"]:
+            print "ETF", portfolio[entry]["name"]
+            continue
+
+        stocks_csv[portfolio[entry]["symbol"]] =  [portfolio[entry]["name"], portfolio[entry]["isin"]]
+
+        if "ES" in portfolio[entry]["isin"]:
+            links = getLinksGoogle("site:cincodias.elpais.com/mercados/empresas/ ", portfolio[entry], " recomendaciones")
+            stocks_csv[portfolio[entry]["symbol"]].append(links[0])
+        else: 
+            links = getLinksGoogle("site:https://www.wsj.com/market-data/quotes/ ", portfolio[entry], " research-ratings")
+            stocks_csv[portfolio[entry]["symbol"]].append(links[0])
+
+
+
+
+    f = open(datadir+"Portfolio_dict.pkl","wb")
+    pickle.dump(stocks_csv,f)
+    f.close()
