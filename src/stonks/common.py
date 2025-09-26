8486c332128403e69fc56d20eee8e626a50bd486
pmatorras <pablo.matorras@cern.ch>
2025-09-26 11:29:31 +0200

    update requirements

--- src/stonks/common.py
+++ src/stonks/common.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 ROOT_DIR = Path(__file__).resolve().parents[2]
 DATA_DIR = ROOT_DIR /"data"
-CACHE_DIR = ROOT_DIR / "cache"
+FIGURE_DIR = ROOT_DIR / "figures"
 LOGS_DIR = ROOT_DIR / "logs"
 SP500NM_FILE = DATA_DIR/"sp500_list.csv"
 SP500MARKET_FILE = DATA_DIR/"sp500_values.csv"
 print(DATA_DIR, SP500NM_FILE,SP500MARKET_FILE)
-for p in (DATA_DIR):
+for p in (DATA_DIR,FIGURE_DIR, LOGS_DIR):
     p.mkdir(parents=True, exist_ok=True)
